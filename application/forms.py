ccdc213ed39e80f96cd952f47b10a6aaefaf1429
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-06-20 21:38:46 +0300

    Add `EqualTo` validation and markup.

--- application/forms.py
+++ application/forms.py
@@ -1,12 +1,16 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class SignUpForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired(), Length(min=6)])
-    password_confirm = PasswordField('password_confirm', validators=[DataRequired(), Length(min=6)])
+    password_confirm = PasswordField('password_confirm', validators=[
+        DataRequired(),
+        Length(min=6),
+        EqualTo('password', message='Passwords must match')
+    ])
 
 
 class SignInForm(Form):
