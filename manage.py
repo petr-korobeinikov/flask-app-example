b35c8b04d5951499a5c28f806ce66c002fa093af
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-06-25 09:56:23 +0300

    Add `fixtures` usage example.

--- manage.py
+++ manage.py
@@ -7,5 +7,24 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def load_fixtures():
+    """
+    Loads database fixtures
+    """
+    import glob
+    from flask_fixtures.loaders import YAMLLoader
+    from flask_fixtures import load_fixtures
+
+    db.drop_all()
+    db.create_all()
+
+    for fixture_dir in app.config.get('FIXTURES_DIRS', ['./fixtures/']):
+        for fixture_file in glob.glob(fixture_dir + '/*.yml'):
+            fixtures = YAMLLoader().load(fixture_file)
+            load_fixtures(db, fixtures)
+
+
 if __name__ == '__main__':
     manager.run()
