70b3b866f3076728df3b460e5cc0ca6c2617bbbb
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-07-02 18:30:01 +0300

    Move static folder outside of application folder.

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static')
 app.config.from_object('application.config.{}'.format(os.environ['APP_SETTINGS']))
 
 db = SQLAlchemy(app)
